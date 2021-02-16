602a5234cf49d54a47a9192824019f68e16fbff3
François BANITZ <francois.banitz@gmail.com>
2021-02-16 11:41:18 +0100

    fixed HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Projets;
+use App\Repository\ProjetsRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
@@ -14,7 +16,7 @@ class HomeController extends AbstractController
     public function index(ProjetsRepository $projetsRepository): Response
     {
         $projets = $projetsRepository->findAll();
-        
+
         return $this->render('home/index.html.twig', [
             'projets' => $projets,
         ]);
